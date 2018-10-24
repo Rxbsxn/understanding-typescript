1e1f1d55d63bb8f89a8f87f5ebfc48d4ab32ec75
Robert Harężlak <harezlak.robert7@gmail.com>
2018-10-24 22:06:48 +0200

    Config & new types

---
+++ typescript.js
@@ -0,0 +1,14 @@
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(4, 4));
+var isCool = true; // boolean
+// Arrays
+var array = ["1", "2", "3"];
+var array2 = ["2", "1", "3"];
+// Objects
+var simpleObject = {
+    a: 1
+};
+var undefinedValue = undefined;
+var nullValue = null;
--- typescript.ts
+++ typescript.ts
@@ -3,3 +3,21 @@ const sum = (a: number, b: number) => {
 }
 
 console.log(sum(4, 4));
+
+
+let isCool: boolean = true; // boolean
+
+
+// Arrays
+let array: string[] = ["1", "2", "3"];
+let array2: Array<string> = ["2", "1", "3"];
+
+
+// Objects
+
+let simpleObject: object = {
+  a: 1
+}
+
+let undefinedValue: undefined = undefined;
+let nullValue: null = null;
